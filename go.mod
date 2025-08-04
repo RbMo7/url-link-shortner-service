8becae5817b5e6d44a6744e1d1111f0461dc4f79
RbMo7 <rbmohit1283@gmail.com>
2025-08-04 15:56:59 +0545

    Initiate http request

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module urlshortener
 
 go 1.24.4
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
-func main(){
-	fmt.Println("Hello, World!")
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	port := os.Getenv("PORT")
+	baseURL := os.Getenv("BASE_URL")
+	dbHost := os.Getenv("DB_HOST")
+
+	fmt.Printf("Server will run on port: %s\n", port)
+	fmt.Printf("Base URL is: %s\n", baseURL)
+	fmt.Printf("Database host is: %s\n", dbHost)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to the URL Shortener!")
+		fmt.Printf("Received request: %s %s\n", r.Method, r.URL.Path)
+	})
+
+	http.ListenAndServe(":"+port, nil)
 }
